1198f1e06f688cad1042a9788c84f70739963050
KolourrMusic <>
2022-06-07 19:55:39 +0000

    June 7 codewars

---
+++ June 7/Multiply the strings in the array.js
@@ -0,0 +1,9 @@
+You received an array with two strings. Create a function that will return their product as a string. E.g.
+
+arrMultiply(['9','6']) should return '54'
+
+
+function arrMultiply(arr){
+   return (Number(arr[0]) * Number(arr[1])).toString()
+   
+}
