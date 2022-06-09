4522aff4cc424b22f8a145635cff22f5b46d68eb
Bruce <contact@estimatebodyfat.com>
2022-06-09 00:19:45 -0400

    update

--- June 8/Sort Arrays (Ignoring Case).js
+++ June 8/Sort Arrays (Ignoring Case).js
@@ -2,3 +2,17 @@ Sort the given array of strings in alphabetical order, case insensitive. For exa
 
 ["Hello", "there", "I'm", "fine"]  -->  ["fine", "Hello", "I'm", "there"]
 ["C", "d", "a", "B"])              -->  ["a", "B", "C", "d"]
+
+
+var sortme = function( names ){
+    return names.sort((a,b) => { return a.toString().toLowerCase() > b.toString().toLowerCase() ? 1 : -1 });
+ }
+ 
+
+
+ sortme = function( names ){
+    return names.sort(function(first, second) {
+      return first.toLowerCase().localeCompare(second.toLowerCase())
+    })
+  }
+  
