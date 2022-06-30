d12b2db1a8f3975878afa2c103d1e1c8309536e9
Bruce <contact@estimatebodyfat.com>
2022-06-30 15:48:47 -0400

    June 30 codewars

--- June 29/Assemble string.js
+++ June 29/Assemble string.js
@@ -22,4 +22,17 @@ input = [
   "a*cd*"
 ]
 result = "a#cd#"
+
+
+function assembleString(array){
+  if(array.length == 0) return ''
+  let word = [...array[0]]
   
+  for (let i = 1; i < array.length; i++)
+    for (let j = 0; j < array[i].length; j++)
+      if (array[i][j] !== '*')
+        word[j] = array[i][j];
+
+  return word.join('').replace(/\*/g, '#')
+  
+}
---
+++ June 30/Inside Out Strings.js
@@ -0,0 +1,33 @@
+You are given a string of words (x), for each word within the string you need to turn the word 'inside out'. By this I mean the internal letters will move out, and the external letters move toward the centre.
+
+If the word is even length, all letters will move. If the length is odd, you are expected to leave the 'middle' letter of the word where it is.
+
+An example should clarify:
+
+'taxi' would become 'atix' 'taxis' would become 'atxsi'
+
+function insideOut(x){
+    let arr = x.split(" ")
+    let newArr = []
+    
+    for(let i=0; i<arr.length; i++){
+      if(arr[i].length > 3){
+        let half = Math.ceil(arr[i].length/2)
+        let lowerhalf = Math.floor(arr[i].length/2)
+        if(arr[i].length % 2 == 0){ 
+          newArr.push(arr[i].slice(0,half).split("").reverse("").join("") +  arr[i].slice(half).split("").reverse("").join("") )
+        }
+        else{
+          newArr.push(arr[i].slice(0,lowerhalf).split("").reverse("").join("") +  arr[i].slice(lowerhalf, lowerhalf+1) + arr[i].slice(lowerhalf+1).split("").reverse("").join("") )
+        }
+      }
+      
+      else{
+        newArr.push(arr[i])
+      }
+      
+    }
+    
+  return newArr.join(" ")
+  }
+   
