14096e094118f150c0ed1502b50db6888a95e0ac
Bruce <contact@estimatebodyfat.com>
2022-10-16 14:47:06 -0400

    update

--- August  21/Reverse every other word in the string.js
+++ August  21/Reverse every other word in the string.js
@@ -1,1 +1,21 @@
-fswedfwedw
+// Reverse every other word in a given string, then return the string. Throw away any leading or trailing whitespace, while ensuring there is exactly one space between each word. Punctuation marks should be treated as if they are a part of the word in this kata.
+
+function reverse(str){
+
+    let arr = str.trim().split(" ")
+
+    let reverse = arr.map((item, index) => {
+
+      if(index === 0 || index % 2 ===0){
+        return item
+      }
+      else{
+         return item.split("").reverse().join("")
+      }
+
+      })
+
+    return reverse.join(" ")
+
+
+  }
